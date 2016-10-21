14ed21ec6b0ef2e8a64aef891f82b917455b7cd2
root <root@0e79a5544be9>
2016-10-21 09:56:58 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "AllTypeServicesforservdefs",
+  "homePage" : "Main",
+  "name" : "AllTypeServicesforservdefs",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
